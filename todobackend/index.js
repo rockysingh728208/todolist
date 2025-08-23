af61d87794d5b858a5ad6b185eb8ff030ba42a6e
rockysingh728208 <sagarkashyap9155@example.com>
2025-08-24 02:10:45 +0530

    wtf

--- todobackend/index.js
+++ todobackend/index.js
@@ -1,24 +1,4 @@
-// import express from 'express'
-// import dotenv from 'dotenv'
-// import cors from 'cors'
-// import { connectDb } from './config/db.js'
-// import todoRoutes from './routes/todoRoutes.js';
 
-// const app = express()
-// app.use(express.json())
-// // const port = 3000
-// app.use(cors());
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// connectDb();
-// app.use('/api/todos', todoRoutes);
-
-// app.listen(process.env.PORT, () => {
-//   console.log(`Example app listening on port ${process.env.PORT || 3000}`)
-// })
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -29,20 +9,16 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-// ✅ CORS allow only frontend URL
+
 app.use(cors({
   origin: "https://todovidya-frontend.vercel.app"
 }));
-
-// Test route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
 connectDb();
 app.use('/api/todos', todoRoutes);
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- todobackend/routes/todoRoutes.js
+++ todobackend/routes/todoRoutes.js
@@ -3,9 +3,9 @@ import { createTodo, getAllTodos, getTodoById, deleteTodo } from "../controllers
 
 const router = express.Router();
 
-router.post("/create", createTodo);        // Create Todo
-router.get("/", getAllTodos);              // Get All Todos
-router.get("/:id", getTodoById);           // Get Todo by ID
-router.delete("/:id", deleteTodo);         // Delete Todo
+router.post("/create", createTodo);  
+router.get("/", getAllTodos);              
+router.get("/:id", getTodoById);          
+router.delete("/:id", deleteTodo);         
 
 export default router;
